4a7564533b61d29b4eae29f881d74906842d5d85
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-29 01:08:31 +0500

    create abs. models

--- server/goods/models.py
+++ server/goods/models.py
@@ -1,16 +1,47 @@
-import math
-
 from django.db import models
+from uuid import uuid4
+from slugify import slugify
 
 
-class Category(models.Model):
+def generate_uuid() -> str:
+    return str(uuid4())
+
+
+class UUIDSlugModel(models.Model):
+    """Abstract model for auto-slug field."""
+    id = models.CharField(primary_key=True, max_length=255, default=generate_uuid)
+    slug = models.SlugField(unique=True, db_index=True, blank=True)
+
+    objects = models.Manager()
+
+    def save(self, **kwargs):
+        """Method to generate slug field."""
+        field = getattr(self, self.SlugMeta.slug_on_field)
+        self.slug = slugify(field)
+        return super().save(**kwargs)
+
+    class SlugMeta:
+        slug_on_field: str
+
+    class Meta:
+        abstract = True
+
+
+class Category(UUIDSlugModel):
     name = models.CharField(max_length=120, db_index=True, unique=True)
 
     def __str__(self):
         return self.name
 
+    class SlugMeta:
+        slug_on_field = "name"
 
-class Subcategory(models.Model):
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+
+class Subcategory(UUIDSlugModel):
     category = models.ForeignKey(
         "Category",
         on_delete=models.CASCADE,
@@ -21,6 +52,13 @@ class Subcategory(models.Model):
     def __str__(self):
         return f"{self.name} ({self.category.name})"
 
+    class SlugMeta:
+        slug_on_field = "name"
+
+    class Meta:
+        verbose_name = "Subcategory"
+        verbose_name_plural = "Subcategories"
+
 
 class CountryChoices(models.TextChoices):
     Russia = "RUSSIA", "Russia"
@@ -28,32 +66,41 @@ class CountryChoices(models.TextChoices):
     Korea = "KOREA", "Korea"
 
 
-class Good(models.Model):
+class Good(UUIDSlugModel):
     subcategory = models.ForeignKey(
         "Subcategory",
         on_delete=models.CASCADE,
         related_name="goods"
     )
-    name = models.CharField(max_length=120, db_index=True)
-    model = models.CharField(max_length=130, db_index=True)
-    brand = models.ForeignKey("Brand", on_delete=models.CASCADE)
+    title = models.CharField(max_length=120, db_index=True)
     description = models.TextField()
+    brand = models.ForeignKey("Brand", on_delete=models.CASCADE, null=True, blank=True)
     images = models.ManyToManyField(to="self", through="GoodImage")
-    made_in_country = models.CharField(max_length=100, choices=CountryChoices.choices)
-    amount = models.IntegerField()
+    photo = models.ImageField(upload_to="goods_img/")
+    made_in_country = models.CharField(max_length=100, choices=CountryChoices.choices, null=True, blank=True)
+    amount = models.PositiveIntegerField(default=1)
     price = models.DecimalField(decimal_places=2, max_digits=12)
-
-    objects = models.Manager()
+    discount = models.PositiveIntegerField(null=True, blank=True)
 
     @property
     def available(self):
         return bool(self.amount)
 
+    @property
+    def total_price(self) -> float:
+        if self.discount is None:
+            return float(self.price)   # type: ignore
+        return (1 - (self.discount/100)) * float(self.price)   # type: ignore
+
+    class SlugMeta:
+        slug_on_field = "title"
+
     class Meta:
-        unique_together = (("name", "model", "brand"), )
+        verbose_name = "Good"
+        verbose_name_plural = "Goods"
 
 
-class Brand(models.Model):
+class Brand(UUIDSlugModel):
     name = models.CharField(max_length=120, db_index=True, unique=True)
     description = models.TextField()
     country = models.CharField(max_length=100, choices=CountryChoices.choices)
@@ -61,16 +108,26 @@ class Brand(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name = "Brand"
+        verbose_name_plural = "Brands"
 
-class GoodImage(models.Model):
+
+class GoodImage(UUIDSlugModel):
     good = models.ForeignKey(
         "Good",
         on_delete=models.CASCADE
     )
     image = models.ImageField()
 
+    objects = models.Manager()
 
-class DescriptionItem(models.Model):
+    class Meta:
+        verbose_name = "Good Image"
+        verbose_name_plural = "Good Images"
+
+
+class DescriptionItem(UUIDSlugModel):
 
     class DescriptionItemTagChoices(models.TextChoices):
         size = "SIZE", "Size"
@@ -86,5 +143,11 @@ class DescriptionItem(models.Model):
     value = models.CharField(max_length=100)
     tag = models.CharField(choices=DescriptionItemTagChoices.choices, max_length=100)
 
+    objects = models.Manager()
+
     def __str__(self):
         return f"{self.key}: {self.value} ({self.tag})"
+
+    class Meta:
+        verbose_name = "Description Item"
+        verbose_name_plural = "Description Items"
