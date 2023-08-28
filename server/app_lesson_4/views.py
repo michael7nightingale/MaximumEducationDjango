82e418fcc4a41c71b2ac9132bcfa314288b85f27
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-28 12:18:14 +0500

    delete homewaork application

--- server/app_lesson_4/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Homework for lesson 4.")
-
-
-index = lambda request: HttpResponse("Homework for lesson 4.")  # why not?
