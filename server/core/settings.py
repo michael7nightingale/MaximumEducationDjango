ffa3df4656e216e9bb0d1be6f42564d7a0e8ac94
michael7nightingale <suslanchikmopl@gmail.com>
2023-08-29 01:05:48 +0500

    add media to settings

--- server/core/settings.py
+++ server/core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,3 +115,9 @@ STATIC_URL = 'static/'
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+LOGIN_URL = "/users/login"
+LOGIN_REDIRECT_URL = "/"
--- server/core/urls.py
+++ server/core/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 
 urlpatterns = [
@@ -8,4 +10,4 @@ urlpatterns = [
     path('', include("home.urls")),
     path('users/', include("users.urls")),
 
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
